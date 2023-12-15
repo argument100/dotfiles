de814c03980abae971a657415314e785a45e5064
ono_tatsuya <web-setting001@ozo-inc.com>
2023-12-16 01:33:05 +0900

    builtin-lspへ置き換え

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -31,7 +31,6 @@ return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
-      -- require('lualine').setup( require "plugins.lualine" )
       require('lualine').setup({
         options = {
           theme = 'dracula-nvim'
@@ -74,7 +73,7 @@ return {
     main = "ibl",
     config = function()
       require('ibl').setup({
-        indent = { highlight = highlight, char = "▏" },
+        indent = { char = "▏" },
       })
     end
   },
@@ -124,18 +123,130 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    config = function () 
+    config = function ()
       local configs = require("nvim-treesitter.configs")
 
       configs.setup({
         ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "html", "css", "scss", "vue", "go" },
         sync_install = false,
         highlight = { enable = true },
-        indent = { enable = true },  
+        indent = { enable = true },
       })
     end
   },
-  require "plugins.cocnvim",
+  {
+    'williamboman/mason.nvim',
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = {
+      { "williamboman/mason.nvim" },
+      { "neovim/nvim-lspconfig" },
+      { "hrsh7th/nvim-cmp" },
+    },
+    config = function()
+      require("mason-lspconfig").setup()
+      require("mason-lspconfig").setup_handlers {
+        function (server_name) -- default handler (optional)
+          local on_attach = function(client, bufnr)
+
+            -- LSPが持つフォーマット機能を無効化する
+            client.server_capabilities.documentFormattingProvider = false
+
+            -- 下記ではデフォルトのキーバインドを設定しています
+            -- ほかのLSPプラグインを使う場合（例：Lspsaga）は必要ないこともあります
+
+            -- keyboard shortcut
+            local set = vim.keymap.set
+            set("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>")
+            set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
+            set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>')
+            set("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>")
+            set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>')
+            set("n", "gt", "<cmd>lua vim.lsp.buf.type_definition()<CR>")
+            set("n", "gn", "<cmd>lua vim.lsp.buf.rename()<CR>")
+            set("n", "ga", "<cmd>lua vim.lsp.buf.code_action()<CR>")
+            set("n", "gh", "<cmd>lua vim.lsp.buf.signature_help()<CR>")
+            set("n", "<space>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>")
+            set("n", "g]", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>")
+            set("n", "g[", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>")
+          end
+
+          -- 補完プラグインであるcmp_nvim_lspをLSPと連携
+          local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+          require("lspconfig")[server_name].setup {
+            on_attach = on_attach, --keyバインドなどの設定を登録
+            capabilities = capabilities, --cmpを連携
+          }
+        end,
+      }
+    end
+  },
+  {
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'onsails/lspkind.nvim',
+    },
+    config = function()
+      -- 3. completion (hrsh7th/nvim-cmp)
+      -- Lspkindのrequire
+      local lspkind = require 'lspkind'
+      --補完関係の設定
+      local cmp = require("cmp")
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            vim.fn["vsnip#anonymous"](args.body)
+          end,
+        },
+        sources = {
+          { name = "nvim_lsp" },--ソース類を設定
+          { name = "buffer" },
+          { name = "path" },
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-p>"] = cmp.mapping.select_prev_item(), --Ctrl+pで補完欄を一つ上に移動
+          ["<C-n>"] = cmp.mapping.select_next_item(), --Ctrl+nで補完欄を一つ下に移動
+          ['<C-l>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.abort(),
+          ["<C-y>"] = cmp.mapping.confirm({ select = true }),--Ctrl+yで補完を選択確定
+        }),
+        experimental = {
+          ghost_text = false,
+        },
+        -- Lspkind(アイコン)を設定
+        formatting = {
+          format = lspkind.cmp_format({
+            mode = 'symbol', -- show only symbol annotations
+            maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+            ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+            -- The function below will be called before any actual modifications from lspkind
+            -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+          })
+        }
+      })
+
+      cmp.setup.cmdline('/', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = 'buffer' } --ソース類を設定
+        }
+      })
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = "path" }, --ソース類を設定
+        },
+      })
+    end
+  },
   require "plugins.telescope",
 
 }
