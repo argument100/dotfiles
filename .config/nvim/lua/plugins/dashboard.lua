1e35b3356be8b1f26ef6e8f7bc1adfab203f244b
argument100 <100blendy@gmail.com>
2023-12-19 21:48:48 +0900

    dashboard-nvimを別ファイルに切り出す

---
+++ .config/nvim/lua/plugins/dashboard.lua
@@ -0,0 +1,29 @@
+return {
+  'nvimdev/dashboard-nvim',
+  event = 'VimEnter',
+  config = function()
+    require('dashboard').setup({
+      config = {
+        header = {
+            [[ ██████╗ ███████╗ ██████╗ ]],
+            [[██╔═══██╗╚══███╔╝██╔═══██╗]],
+            [[██║   ██║  ███╔╝ ██║   ██║]],
+            [[██║   ██║ ███╔╝  ██║   ██║]],
+            [[╚██████╔╝███████╗╚██████╔╝]],
+            [[ ╚═════╝ ╚══════╝ ╚═════╝ ]],
+            [[                          ]],
+        },
+        shortcut = {
+          { desc = '󰊳 Update', group = '@property', action = 'Lazy update', key = 'u' },
+          {
+            desc = ' dotfiles',
+            group = 'Number',
+            action = 'cd ~/dotfiles/.config/nvim/\nNvimTreeOpen',
+            key = 'd',
+          },
+        },
+      }
+    })
+  end,
+  dependencies = { {'nvim-tree/nvim-web-devicons'}}
+}
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,25 +1,5 @@
 return {
-  {
-    'nvimdev/dashboard-nvim',
-    event = 'VimEnter',
-    config = function()
-      require('dashboard').setup({
-        config = {
-          header = {
-            [[                          ]],
-            [[ ██████╗ ███████╗ ██████╗ ]],
-            [[██╔═══██╗╚══███╔╝██╔═══██╗]],
-            [[██║   ██║  ███╔╝ ██║   ██║]],
-            [[██║   ██║ ███╔╝  ██║   ██║]],
-            [[╚██████╔╝███████╗╚██████╔╝]],
-            [[ ╚═════╝ ╚══════╝ ╚═════╝ ]],
-            [[                          ]],
-          }
-        }
-      })
-    end,
-    dependencies = { {'nvim-tree/nvim-web-devicons'}}
-  },
+  require "plugins.dashboard",
   { -- colorscheme
     'Mofiqul/dracula.nvim',
     config = function()
