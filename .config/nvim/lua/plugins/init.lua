52e243f42d4c045d4d2f636f466fa79ae90894d8
argument100 <100blendy@gmail.com>
2023-12-26 22:16:55 +0900

    Separate orgmode from init.lua

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -301,22 +301,5 @@ return {
   },
 
   require "plugins.telescope",
-  {
-    'nvim-orgmode/orgmode',
-    dependencies = {
-      { 'nvim-treesitter/nvim-treesitter', lazy = true },
-    },
-    event = 'VeryLazy',
-    config = function()
-      -- Load treesitter grammar for org
-      require('orgmode').setup_ts_grammar()
-
-      -- Setup orgmode
-      require('orgmode').setup({
-        org_agenda_files = '~/orgfiles/**/*',
-        org_default_notes_file = '~/orgfiles/refile.org',
-      })
-    end,
-  }
-
+  require "plugins.orgmode",
 }
